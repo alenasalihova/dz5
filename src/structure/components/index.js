87cabb1fcb78f30e311b33f69a620445d3c58e19
Salihova Alena <alexx9202@gmail.com>
2024-01-06 01:17:20 +0200

    second correct

--- src/structure/components/index.js
+++ src/structure/components/index.js
@@ -1,17 +1,6 @@
-import { Component } from "react";
-
 export { Comments } from './Comments';
 export { Counter } from './Counter';
 export { Instruction } from './Instruction';
 export { MyEvents } from './MyEvents';
 export { Posts } from './Posts';
 export { Users } from './User';
-
-class Comp extends Component {
-
-    render () {
-        return <div></div>
-    }
-}
-
-export default Comp;
--- src/structure/index.js
+++ src/structure/index.js
@@ -1,15 +1,4 @@
-import { Component } from "react";
-
 export { Content } from './Content';
 export { Footer } from './Footer';
 export { Header } from './Header';
 export { Sidebar } from './Sidebar';
-
-class Structure extends Component {
-
-    render () {
-        return <div></div>
-    }
-}
-
-export default Structure;
